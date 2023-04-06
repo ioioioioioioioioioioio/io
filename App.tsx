0df4040fadf9d8e06cbfbeed135a5d40794efe16
Wojciech Dróżdż <behenate@gmail.com>
2023-04-06 23:19:57 +0200

    Add react navigation with a simple example

--- App.tsx
+++ App.tsx
@@ -1,18 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Test from './screens/PetScreen';
+import FeaturesScreen, { Feature } from './screens/FeaturesScreen';
+import PetScreen from './screens/PetScreen';
+
+export type RootStackParamList = {
+  FeaturesScreen: { features: Feature[] };
+  PetScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const featureList: Feature[] = [
+  { title: 'Pet Screen', screenName: 'PetScreen' },
+  { title: 'Pet Screen', screenName: 'PetScreen' },
+  { title: 'Pet Screen', screenName: 'PetScreen' },
+  { title: 'Pet Screen', screenName: 'PetScreen' },
+];
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Test />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="FeaturesScreen"
+          component={FeaturesScreen}
+          initialParams={{ features: featureList }}
+        />
+        <Stack.Screen name="PetScreen" component={PetScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
---
+++ screens/FeaturesScreen.tsx
@@ -0,0 +1,36 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StyleSheet, FlatList } from 'react-native';
+
+import { RootStackParamList } from '../App';
+import ListElem from '../components/ListElem';
+
+export type Feature = {
+  title: string;
+  // We should be able to find a better way to do this other than listing all possible screen names
+  screenName: 'PetScreen';
+};
+
+type FeaturesScreenProps = NativeStackScreenProps<RootStackParamList, 'FeaturesScreen'>;
+
+export default function FeaturesScreen({
+  route: {
+    params: { features },
+  },
+  navigation,
+}: FeaturesScreenProps) {
+  return (
+    <FlatList
+      style={styles.container}
+      data={features}
+      renderItem={({ item }) => (
+        <ListElem title={item.title} onPress={() => navigation.navigate(item.screenName)} />
+      )}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#e6e6e6',
+  },
+});
