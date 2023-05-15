19562d5b7414d3c114ad0efc057d211e85d6685f
MaOlszewska <marto12@wp.pl>
2023-05-15 13:18:50 +0200

    Added Screen dedicated to editing chosen Entry

--- App.tsx
+++ App.tsx
@@ -7,12 +7,15 @@ import AddEntryScreen from './screens/AddEntryScreen';
 import FeaturesScreen, { Feature } from './screens/FeaturesScreen';
 import InfoScreen from './screens/InfoScreen';
 import PetScreen from './screens/PetScreen';
+import EditScreen from "./screens/EditScreen";
+import React from "react";
 
 export type RootStackParamList = {
   FeaturesScreen: { features: Feature[] };
   PetScreen: undefined;
   InfoScreen: undefined;
   AddEntryScreen: undefined;
+  EditScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -21,6 +24,7 @@ const featureList: Feature[] = [
   { title: 'Pet Screen', screenName: 'PetScreen' },
   { title: 'Info Screen', screenName: 'InfoScreen' },
   { title: 'Add Entry', screenName: 'AddEntryScreen' },
+  { title: 'Edit Entry', screenName: 'EditScreen' },
 ];
 
 export default function App() {
@@ -36,6 +40,7 @@ export default function App() {
           <Stack.Screen name="InfoScreen" component={InfoScreen} />
           <Stack.Screen name="PetScreen" component={PetScreen} />
           <Stack.Screen name="AddEntryScreen" component={AddEntryScreen} />
+          <Stack.Screen name="EditScreen" component={EditScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
